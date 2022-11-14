fe675582cd2db8aa929ef541b5d2801bda7cbf6b
weizhenkoh <wei.zhen.koh@fusionexgroup.com>
2022-11-14 15:26:49 +0800

    Update NewBehaviourScript.cs

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
